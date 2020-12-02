3fc349467fbefae26f79e7c4c088f1d1758399f0
hoangtrungchinh <chinhguitar@gmail.com>
2020-12-02 10:30:58 +0700

    add verion TED with only BPE

--- examples/translation/prepare-en-vi-iwslt-only-BPE.sh
+++ examples/translation/prepare-en-vi-iwslt-only-BPE.sh
@@ -18,7 +18,7 @@ BPE_TOKENS=10000
 URL=https://github.com/hoangtrungchinh/clc_data/raw/master/en_vi_iwslt.tar.gz
 GZ=en_vi_iwslt.tar.gz
 
-prefix=ed-iwslt-
+prefix=bpe-iwslt-
 src=en
 tgt=vi
 lang=en-vi
@@ -36,17 +36,11 @@ if [ -f $GZ ]; then
     echo "Data successfully downloaded."
 else
     echo "Data not successfully downloaded."
-    exit
 fi
 
 mkdir $lang
 tar -xvf $GZ -C $lang
 cd ..
-
-python clc_fairseq/examples/translation/script_ed.py -i $orig/$lang/train.$src -o $orig/$lang/train2.$src
-cat $orig/$lang/train.$src > $orig/$lang/train.$src.bak
-cat $orig/$lang/train2.$src > $orig/$lang/train.$src
-
 
 
 echo "pre-processing train data..."
@@ -54,33 +48,22 @@ for l in $src $tgt; do
     f=train.$l
     tok=train.tags.$lang.tok.$l
 
-    cat $orig/$lang/$f | \
-    perl $TOKENIZER -threads 8 -l $l > $tmp/$tok
+    # BO BUOC NAY
+    # cat $orig/$lang/$f | \
+    # perl $TOKENIZER -threads 8 -l $l > $tmp/$tok
+    cat $orig/$lang/$f  > $tmp/$tok
     echo ""
 done
-# ==> replace abbreviations
-# ==> create 2 file: "train.tags.en-vi.tok.vi" and "train.tags.en-vi.tok.en"
-# pre-processing train data...
-# Tokenizer Version 1.1
-# Language: en
-# Number of threads: 8
 
-# Tokenizer Version 1.1
-# Language: vi
-# Number of threads: 8
-# WARNING: No known abbreviations for language 'vi', attempting fall-back to English version...
-#
 
-perl $CLEAN -ratio 1.5 $tmp/train.tags.$lang.tok $src $tgt $tmp/train.tags.$lang.clean 1 175
-# ==>
-# ==> create 2 file: "train.tags.en-vi.clean.vi" and "train.tags.en-vi.clean.en"
-# clean-corpus.perl: processing ed-iwslt-ed-iwslt-tokenized.en-vi/tmp/train.tags.en-vi.tok.en & .vi to ed-iwslt-ed-iwslt-tokenized.en-vi/tmp/train.tags.en-vi.clean, cutoff 1-175, ratio 1.5
-# ..........(100000).
-# Input sentences: 117055  Output sentences:  18604
+
+# BO BUOC NAY
+# perl $CLEAN -ratio 1.5 $tmp/train.tags.$lang.tok $src $tgt $tmp/train.tags.$lang.clean 1 175
+
 
 
 for l in $src $tgt; do
-    perl $LC < $tmp/train.tags.$lang.clean.$l > $tmp/train.tags.$lang.$l
+    perl $LC < $tmp/train.tags.$lang.tok.$l > $tmp/train.tags.$lang.$l
 done
 # ==> Lowercase and create 2 file "train.tags.en-vi.vi" and "train.tags.en-vi.en" in tmp folder
 
@@ -91,8 +74,10 @@ for l in $src $tgt; do
     f=IWSLT15.TED.tst2015.$lang.$l
     tok=test.tags.$lang.tok.$l
 
-    cat $orig/$lang/$f | \
-    perl $TOKENIZER -threads 8 -l $l > $tmp/$tok
+    # BO BUOC NAY
+    # cat $orig/$lang/$f | \
+    # perl $TOKENIZER -threads 8 -l $l > $tmp/$tok
+    cat $orig/$lang/$f  > $tmp/$tok
     perl $LC < $tmp/$tok > $tmp/test.tags.$lang.$l
     echo ""
 done
@@ -152,19 +137,19 @@ TEXT=$prefix"tokenized.en-vi" \
     --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
     --destdir $prefix"data-bin/tokenized.en-vi" \
     --workers 20
-# 2020-12-01 04:54:02 | INFO | fairseq_cli.preprocess | [en] Dictionary: 7864 types
-# 2020-12-01 04:54:04 | INFO | fairseq_cli.preprocess | [en] ed-iwslt-tokenized.en-vi/train.en: 17796 sents, 1056375 tokens, 0.0% replaced by <unk>
-# 2020-12-01 04:54:04 | INFO | fairseq_cli.preprocess | [en] Dictionary: 7864 types
-# 2020-12-01 04:54:04 | INFO | fairseq_cli.preprocess | [en] ed-iwslt-tokenized.en-vi/valid.en: 808 sents, 49782 tokens, 0.0241% replaced by <unk>
-# 2020-12-01 04:54:04 | INFO | fairseq_cli.preprocess | [en] Dictionary: 7864 types
-# 2020-12-01 04:54:05 | INFO | fairseq_cli.preprocess | [en] ed-iwslt-tokenized.en-vi/test.en: 1080 sents, 26133 tokens, 0.0191% replaced by <unk>
-# 2020-12-01 04:54:05 | INFO | fairseq_cli.preprocess | [vi] Dictionary: 4952 types
-# 2020-12-01 04:54:06 | INFO | fairseq_cli.preprocess | [vi] ed-iwslt-tokenized.en-vi/train.vi: 17796 sents, 723197 tokens, 0.0% replaced by <unk>
-# 2020-12-01 04:54:06 | INFO | fairseq_cli.preprocess | [vi] Dictionary: 4952 types
-# 2020-12-01 04:54:07 | INFO | fairseq_cli.preprocess | [vi] ed-iwslt-tokenized.en-vi/valid.vi: 808 sents, 33725 tokens, 0.0682% replaced by <unk>
-# 2020-12-01 04:54:07 | INFO | fairseq_cli.preprocess | [vi] Dictionary: 4952 types
-# 2020-12-01 04:54:07 | INFO | fairseq_cli.preprocess | [vi] ed-iwslt-tokenized.en-vi/test.vi: 1080 sents, 28749 tokens, 1.51% replaced by <unk>
-# 2020-12-01 04:54:07 | INFO | fairseq_cli.preprocess | Wrote preprocessed data to ed-iwslt-data-bin/tokenized.en-vi
+# 2020-11-21 10:39:22 | INFO | fairseq_cli.preprocess | [en] Dictionary: 7768 types
+# 2020-11-21 10:39:28 | INFO | fairseq_cli.preprocess | [en] iwslt-tokenized.en-vi/train.en: 109322 sents, 2874159 tokens, 0.0% replaced by <unk>
+# 2020-11-21 10:39:28 | INFO | fairseq_cli.preprocess | [en] Dictionary: 7768 types
+# 2020-11-21 10:39:28 | INFO | fairseq_cli.preprocess | [en] iwslt-tokenized.en-vi/valid.en: 4969 sents, 128336 tokens, 0.00156% replaced by <unk>
+# 2020-11-21 10:39:28 | INFO | fairseq_cli.preprocess | [en] Dictionary: 7768 types
+# 2020-11-21 10:39:29 | INFO | fairseq_cli.preprocess | [en] iwslt-tokenized.en-vi/test.en: 1080 sents, 26128 tokens, 0.0191% replaced by <unk>
+# 2020-11-21 10:39:29 | INFO | fairseq_cli.preprocess | [vi] Dictionary: 6720 types
+# 2020-11-21 10:39:37 | INFO | fairseq_cli.preprocess | [vi] iwslt-tokenized.en-vi/train.vi: 109322 sents, 2956068 tokens, 0.0% replaced by <unk>
+# 2020-11-21 10:39:37 | INFO | fairseq_cli.preprocess | [vi] Dictionary: 6720 types
+# 2020-11-21 10:39:37 | INFO | fairseq_cli.preprocess | [vi] iwslt-tokenized.en-vi/valid.vi: 4969 sents, 131647 tokens, 0.0319% replaced by <unk>
+# 2020-11-21 10:39:37 | INFO | fairseq_cli.preprocess | [vi] Dictionary: 6720 types
+# 2020-11-21 10:39:37 | INFO | fairseq_cli.preprocess | [vi] iwslt-tokenized.en-vi/test.vi: 1080 sents, 28544 tokens, 0.855% replaced by <unk>
+# 2020-11-21 10:39:37 | INFO | fairseq_cli.preprocess | Wrote preprocessed data to data-bin/tokenized.en-vi
 
 # data-bin
 # └── tokenized.en-vi
@@ -187,24 +172,24 @@ TEXT=$prefix"tokenized.en-vi" \
 
 
 # TRAINING
-mkdir -p $prefix"checkpoints/fconv_wmt_en_vi"
+# mkdir -p $prefix"checkpoints/fconv_wmt_en_vi"
 
-CUDA_VISIBLE_DEVICES=0 fairseq-train $prefix"data-bin/tokenized.en-vi" \
-    --arch transformer --share-decoder-input-output-embed \
-    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
-    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 400 \
-    --dropout 0.3 --weight-decay 0.0001 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-    --max-tokens 4096 \
-    --eval-bleu \
-    --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
-    --eval-bleu-detok moses \
-    --eval-bleu-remove-bpe \
-    --eval-bleu-print-samples \
-    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
-    --save-dir $prefix"checkpoints/fconv_wmt_en_vi"
+# CUDA_VISIBLE_DEVICES=0 fairseq-train $prefix"data-bin/tokenized.en-vi" \
+#     --arch transformer --share-decoder-input-output-embed \
+#     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
+#     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 400 \
+#     --dropout 0.3 --weight-decay 0.0001 \
+#     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+#     --max-tokens 4096 \
+#     --eval-bleu \
+#     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
+#     --eval-bleu-detok moses \
+#     --eval-bleu-remove-bpe \
+#     --eval-bleu-print-samples \
+#     --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+#     --max-epoch 1 \
+#     --save-dir $prefix"checkpoints/fconv_wmt_en_vi"
 
-    # --max-epoch 1 \
 
 # --arch
 # Possible choices: transformer, transformer_iwslt_de_en, transformer_wmt_en_de, transformer_vaswani_wmt_en_de_big, transformer_vaswani_wmt_en_fr_big, transformer_wmt_en_de_big, transformer_wmt_en_de_big_t2t, multilingual_transformer, multilingual_transformer_iwslt_de_en, fconv, fconv_iwslt_de_en, fconv_wmt_en_ro, fconv_wmt_en_de, fconv_wmt_en_fr, nonautoregressive_transformer, nonautoregressive_transformer_wmt_en_de, nacrf_transformer, iterative_nonautoregressive_transformer, iterative_nonautoregressive_transformer_wmt_en_de, cmlm_transformer, cmlm_transformer_wmt_en_de, levenshtein_transformer, levenshtein_transformer_wmt_en_de, levenshtein_transformer_vaswani_wmt_en_de_big, levenshtein_transformer_wmt_en_de_big, insertion_transformer, bart_large, bart_base, mbart_large, mbart_base, mbart_base_wmt20, lstm, lstm_wiseman_iwslt_de_en, lstm_luong_wmt_en_de, transformer_lm, transformer_lm_big, transformer_lm_baevski_wiki103, transformer_lm_wiki103, transformer_lm_baevski_gbw, transformer_lm_gbw, transformer_lm_gpt, transformer_lm_gpt2_small, transformer_lm_gpt2_medium, transformer_lm_gpt2_big, transformer_align, transformer_wmt_en_de_big_align, hf_gpt2, hf_gpt2_medium, hf_gpt2_large, hf_gpt2_xl, transformer_from_pretrained_xlm, lightconv, lightconv_iwslt_de_en, lightconv_wmt_en_de, lightconv_wmt_en_de_big, lightconv_wmt_en_fr_big, lightconv_wmt_zh_en_big, lightconv_lm, lightconv_lm_gbw, fconv_self_att, fconv_self_att_wp, fconv_lm, fconv_lm_dauphin_wikitext103, fconv_lm_dauphin_gbw, lstm_lm, roberta, roberta_base, roberta_large, xlm, masked_lm, bert_base, bert_large, xlm_base, s2t_berard, s2t_berard_256_3_3, s2t_berard_512_3_2, s2t_berard_512_5_3, s2t_transformer, s2t_transformer_s, s2t_transformer_sp, s2t_transformer_m, s2t_transformer_mp, s2t_transformer_l, s2t_transformer_lp, wav2vec, wav2vec2, wav2vec_ctc, wav2vec_seq2seq, dummy_model, transformer_lm_megatron, transformer_lm_megatron_11b, transformer_iwslt_de_en_pipeline_parallel, transformer_wmt_en_de_big_pipeline_parallel, model_parallel_roberta, model_parallel_roberta_base, model_parallel_roberta_large
