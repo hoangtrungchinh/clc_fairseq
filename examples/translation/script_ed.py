5ac46a60c84951451ca97b51d0c02a10c8439bff
hoangtrungchinh <chinhguitar@gmail.com>
2020-12-04 08:39:45 +0700

    update

--- examples/translation/script_ed.py
+++ examples/translation/script_ed.py
@@ -1,48 +1,108 @@
-#!/usr/bin/python
-
 import sys, getopt
 import editdistance
 import time
-
+import concurrent.futures
 
 def main(argv):
   start = time.time()
-  inputfile = ''
-  outputfile = ''
+  srcfile = ''
+  tarfile = ''
+  outfile = ''
   saperate = "@@@"
   try:
-    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    opts, args = getopt.getopt(argv,"st:o:",["srcfile=","tarfile=","outfile=", "thread="])
   except getopt.GetoptError:
-    print('test.py -i <inputfile> -o <outputfile>')
+    print('test.py -src <srcfile> -tar <tarfile> -out <outfile>')
     sys.exit(2)
   for opt, arg in opts:
     if opt == '-h':
-      print('test.py -i <inputfile> -o <outputfile>')
+      print('test.py -src <srcfile> -tar <tarfile> -out <outfile>')
       sys.exit()
-    elif opt in ("-i", "--ifile"):
-      inputfile = arg
-    elif opt in ("-o", "--ofile"):
-      outputfile = arg
+    elif opt in ("-s", "--srcfile"):
+      srcfile = arg
+    elif opt in ("-t", "--tarfile"):
+      tarfile = arg
+    elif opt in ("-o", "--outfile"):
+      outfile = arg
+    elif opt in ("-m", "--thread"):
+      thread = arg
 
   print('=== Starting Calculate editdistance')
-  print('Input file is ', inputfile)
-  print('Output file is ', outputfile)
+  print('source file is ', srcfile)
+  print('target file is ', tarfile)
+  print('output file is ', outfile)
 
-  file_read = open(inputfile, 'r')
-  Lines1 = file_read.readlines()
-
-  file_write = open(outputfile, 'w')
-  for l1 in Lines1:
+  def best_simi(str):
     dis = 10000000
+    index = 0
     best_sent = ""
-    for l2 in Lines1:
-      tmp = editdistance.eval(l1, l2)
-      if tmp !=0 and tmp < dis:
-        dis = tmp
-        best_sent = l2
+    with open(srcfile) as src_f2:
+      for j, cnt in enumerate(src_f2):
+        tmp = editdistance.eval(str, cnt)
+        if tmp !=0 and tmp < dis:
+          dis = tmp
+          best_sent = cnt
+          index = j
+      return best_sent, index
 
-    file_write.writelines(l1.strip() + saperate + best_sent)
-  file_write.close()
+
+  def best_simi_multi(str):
+    dis = 10000000
+    index = 0
+    best_sent = ""
+    with open(srcfile) as src_f2:
+      for j, cnt in enumerate(src_f2):
+        tmp = editdistance.eval(str, cnt)
+        if tmp !=0 and tmp < dis:
+          dis = tmp
+          best_sent = cnt
+          index = j
+      a_file = open(tarfile)
+      for position, line in enumerate(a_file):
+        if position == index:
+          return str.strip() + saperate + best_sent.strip()  + saperate + line
+
+
+
+
+# 1 thread
+  if thread=="1":
+    out_file_write = open(outfile, 'w')
+    with open(srcfile) as src_f:
+      for i, cnt1 in enumerate(src_f):
+        best_sent, index = best_simi(cnt1)
+
+        # Get and save content
+        a_file = open(tarfile)
+        for position, line in enumerate(a_file):
+          if position == index:
+            out_file_write.writelines(cnt1.strip() + saperate + best_sent.strip()  + saperate + line)
+            break
+
+    out_file_write.close()
+  else:
+    dict=[]
+    file1 = open(srcfile, 'r')
+    Lines = file1.readlines()
+    out_file_write = open(outfile, 'w')
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers = int(thread)) as executor:
+      res = {executor.submit(best_simi_multi, e): e for e in Lines}
+      # Get and save content
+      # a_file = open(tarfile)
+      # for position, line in enumerate(a_file):
+      #   if position == index:
+      #     out_file_write.writelines(cnt1.strip() + saperate + best_sent.strip()  + saperate + line)
+      #     break
+
+
+      for future in concurrent.futures.as_completed(res):
+        out_file_write.writelines(future.result())
+        # dict.append(future.result())
+        # print(future.result())
+    # print(dict)
+
+    out_file_write.close()
 
 
   done = time.time()
